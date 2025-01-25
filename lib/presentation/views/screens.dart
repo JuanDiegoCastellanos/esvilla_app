ec6927c07e9c158d87a347f26c6e8c9108f13e11
Juan Diego Castellanos <juandiego.castellanosjerez@gmail.com>
2025-01-25 10:58:26 -0500

    JDC: Added the new screen to the list

--- lib/presentation/views/screens.dart
+++ lib/presentation/views/screens.dart
@@ -1,3 +1,4 @@
 export 'package:esvilla_app/presentation/views/home_screen/home_screen.dart';
 export 'package:esvilla_app/presentation/views/login_screen/login_screen.dart';
 export 'package:esvilla_app/presentation/views/splash_screen/splash_screen.dart';
+export 'package:esvilla_app/presentation/views/register_screen/register_screen.dart';
