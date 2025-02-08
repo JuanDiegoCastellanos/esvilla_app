dfad2e14c06b54714885cc3f9798d2494c31093a
Juan Diego Castellanos <juandiego.castellanosjerez@gmail.com>
2025-02-07 23:40:05 -0500

    JDC: New dio configuration to integrate an AuthInterceptor

--- lib/core/config/dio_config.dart
+++ lib/core/config/dio_config.dart
@@ -1,32 +1,31 @@
 import 'package:dio/dio.dart';
 import 'package:esvilla_app/core/config/app_config.dart';
+import 'package:esvilla_app/core/utils/auth_interceptor.dart';
 import 'package:esvilla_app/core/utils/dio_logging_interceptor.dart';
-import 'package:esvilla_app/presentation/providers/auth_token_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final dioClientProvider = Provider(
   (ref) {
-    final token = ref.watch(authTokenProvider);
-    return DioClient(token);
+    return DioClient(ref);
   });
 
 class DioClient {
   final Dio _dio = Dio();
+  final Ref ref;
 
-  DioClient(String? token) {
+  DioClient(this.ref) {
 
     _dio.options = BaseOptions(
       baseUrl: AppConfig.apiUrl,
-      connectTimeout: const Duration(seconds: 10),
-      receiveTimeout: const Duration(seconds: 10),
+      connectTimeout: const Duration(seconds: 100),
+      receiveTimeout: const Duration(seconds: 100),
       contentType: 'application/json',
     );
-
-    if (token != null) {
-      _dio.options.headers['Authorization'] = 'Bearer $token';
-    }
     // Añadir interceptores
-    _dio.interceptors.add(DioLoggingInterceptor());
+    _dio.interceptors.addAll([
+      AuthInterceptor(ref),
+      DioLoggingInterceptor()
+      ]);
   }
 
   Dio get dio => _dio;
