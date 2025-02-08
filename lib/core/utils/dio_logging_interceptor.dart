7d5e1f792083fc5ffb3a614e0ad368034d49f39f
Juan Diego Castellanos <juandiego.castellanosjerez@gmail.com>
2025-02-07 23:47:34 -0500

    JDC: I updated the dio logging cause the new interceptor methods

--- lib/core/utils/dio_logging_interceptor.dart
+++ lib/core/utils/dio_logging_interceptor.dart
@@ -1,28 +1,18 @@
 import 'package:dio/dio.dart';
-import 'package:logger/logger.dart';
+import 'package:esvilla_app/core/config/app_logger.dart';
 
 class DioLoggingInterceptor extends Interceptor {
-  final Logger _logger = Logger();
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    _logger.d("🚀 REQUEST: [${options.method}] ${options.uri}");
-    if (options.data != null) _logger.d("🔍 Body: ${options.data}");
+    AppLogger.d("🚀 REQUEST: [${options.method}] ${options.uri}");
+    if (options.data != null) AppLogger.d("🔍 Body: ${options.data}");
     handler.next(options);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    _logger.i("✅ RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.uri}");
+    AppLogger.i("✅ RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.uri}");
     handler.next(response);
-  }
-
-  @override
-  void onError(DioException err, ErrorInterceptorHandler handler) {
-    _logger.e("❌ ERROR[${err.response?.statusCode}] => ${err.message}");
-    if (err.response?.data != null) {
-      _logger.e("📦 Error Data: ${err.response?.data}");
-    }
-    handler.next(err);
   }
 }
