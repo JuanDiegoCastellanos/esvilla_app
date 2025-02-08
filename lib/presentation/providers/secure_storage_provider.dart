7d47f623d034dc65a307e29fe915b9bdace4d70d
Juan Diego Castellanos <juandiego.castellanosjerez@gmail.com>
2025-02-07 23:52:40 -0500

    JDC: Changed the class to get the provider

--- lib/presentation/providers/secure_storage_provider.dart
+++ lib/presentation/providers/secure_storage_provider.dart
@@ -3,6 +3,6 @@ import 'package:esvilla_app/core/utils/secure_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-final secureStorageServiceProvider = Provider<SecureStorageService>((ref) {
-  return SecureStorageService(const FlutterSecureStorage());
+final secureStorageServiceProvider = Provider<SecureStorageServiceImpl>((ref) {
+  return SecureStorageServiceImpl(const FlutterSecureStorage());
 });
