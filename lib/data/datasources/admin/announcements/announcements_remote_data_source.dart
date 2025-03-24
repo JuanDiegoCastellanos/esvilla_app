04a4fdceebd5717cf95a1e46980bef53e497caee
Juan Diego Castellanos <juandiego.castellanosjerez@gmail.com>
2025-03-24 12:38:27 -0500

    JDC: Added get by ID and getMyPqrs

--- lib/data/datasources/admin/announcements/announcements_remote_data_source.dart
+++ lib/data/datasources/admin/announcements/announcements_remote_data_source.dart
@@ -132,6 +132,44 @@ class AnnouncementsRemoteDataSource {
     }
   }
 
+    /// Gets an announcement by ID from the server.
+  ///
+  /// [token] is the JSON Web Token used to authenticate the request.
+  ///
+  /// [id] is the ID of the announcement to retrieve.
+  ///
+  /// Returns an [AnnouncementModel] representing the requested announcement.
+  ///
+  /// Throws an [AppException] if a network error or unexpected error occurs.
+  Future<AnnouncementModel> getAnnouncementByID(String token, String id) async {
+    try {
+      final response = await _dio.get(
+        '/announcements/$id',
+        options: Options(headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token'
+        }),
+      );
+      AppLogger.i('Response Data: ${response.data}');
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return AnnouncementModel.fromMap(response.data);
+      } else {
+        AppLogger.e(
+            'Failed to get my announcements. Status: ${response.statusCode}, Body: ${response.data}');
+
+        throw Exception(
+            'Failed to get my announcements. Status: ${response.statusCode}, Body: ${response.data}');
+      }
+    } on DioException catch (e) {
+      AppLogger.e('Dio error during get my announcements: $e');
+      throw AppException.fromDioExceptionType(e.type);
+    } catch (e) {
+      AppLogger.e('Unexpected error during get my announcements: $e');
+      throw AppException(code: -1, message: 'Unexpected error occurred');
+    }
+  }
+
   /// Gets all the announcements created by a user from the server, filtered by
   /// their creation date.
   ///
@@ -147,7 +185,7 @@ class AnnouncementsRemoteDataSource {
   /// created by the user in the given range.
   ///
   /// Throws an [AppException] if a network error or unexpected error occurs.
-  Future<List<AnnouncementModel>> getAnnouncementsByCreationDate(
+  Future<List<AnnouncementModel>> getMyAnnouncementsByCreationDate(
       String token, String startDate, String endDate) async {
     try {
       final response = await _dio.get(
@@ -206,7 +244,7 @@ class AnnouncementsRemoteDataSource {
   /// created by the user in the given range.
   ///
   /// Throws an [AppException] if a network error or unexpected error occurs.
-  Future<List<AnnouncementModel>> getAnnouncementsByPublicationDate(
+  Future<List<AnnouncementModel>> getMyAnnouncementsByPublicationDate(
       String token, String startDate, String endDate) async {
     try {
       final response = await _dio.get(
@@ -250,20 +288,15 @@ class AnnouncementsRemoteDataSource {
     }
   }
 
-
-  /// Gets an announcement by ID from the server.
-  ///
-  /// [token] is the JSON Web Token used to authenticate the request.
-  ///
-  /// [id] is the ID of the announcement to retrieve.
-  ///
-  /// Returns an [AnnouncementModel] representing the requested announcement.
-  ///
-  /// Throws an [AppException] if a network error or unexpected error occurs.
-  Future<AnnouncementModel> getAnnouncementByID(String token, String id) async {
+    Future<List<AnnouncementModel>> getAnnouncementsByCreationDate(
+      String token, String startDate, String endDate) async {
     try {
       final response = await _dio.get(
-        '/announcements/$id',
+        '/announcements/creation-range',
+        queryParameters: {
+          'startDate': startDate,
+          'endDate': endDate,
+        },
         options: Options(headers: {
           'Content-Type': 'application/json',
           'Authorization': 'Bearer $token'
@@ -272,19 +305,73 @@ class AnnouncementsRemoteDataSource {
       AppLogger.i('Response Data: ${response.data}');
 
       if (response.statusCode == 200 || response.statusCode == 201) {
-        return AnnouncementModel.fromMap(response.data);
+        // Se asume que response.data es una lista de mapas (JSON)
+        List<dynamic> data = response.data;
+        List<AnnouncementModel> announcements = data
+            .map((json) =>
+                AnnouncementModel.fromMap(json as Map<String, dynamic>))
+            .toList();
+        return announcements;
+        /* final List<dynamic> responseData = response.data;
+        return responseData
+          .map((item) => AnnouncementModel.fromMap(item))
+          .toList(); */
       } else {
         AppLogger.e(
-            'Failed to get my announcements. Status: ${response.statusCode}, Body: ${response.data}');
+            'Failed to get announcements. Status: ${response.statusCode}, Body: ${response.data}');
 
         throw Exception(
-            'Failed to get my announcements. Status: ${response.statusCode}, Body: ${response.data}');
+            'Failed to get announcements. Status: ${response.statusCode}, Body: ${response.data}');
       }
     } on DioException catch (e) {
-      AppLogger.e('Dio error during get my announcements: $e');
+      AppLogger.e('Dio error during get announcements: $e');
       throw AppException.fromDioExceptionType(e.type);
     } catch (e) {
-      AppLogger.e('Unexpected error during get my announcements: $e');
+      AppLogger.e('Unexpected error during get announcements: $e');
+      throw AppException(code: -1, message: 'Unexpected error occurred');
+    }
+  }
+
+    Future<List<AnnouncementModel>> getAnnouncementsByPublishDate(
+      String token, String startDate, String endDate) async {
+    try {
+      final response = await _dio.get(
+        '/announcements/publication-range',
+        queryParameters: {
+          'startDate': startDate,
+          'endDate': endDate,
+        },
+        options: Options(headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token'
+        }),
+      );
+      AppLogger.i('Response Data: ${response.data}');
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        // Se asume que response.data es una lista de mapas (JSON)
+        List<dynamic> data = response.data;
+        List<AnnouncementModel> announcements = data
+            .map((json) =>
+                AnnouncementModel.fromMap(json as Map<String, dynamic>))
+            .toList();
+        return announcements;
+        /* final List<dynamic> responseData = response.data;
+        return responseData
+          .map((item) => AnnouncementModel.fromMap(item))
+          .toList(); */
+      } else {
+        AppLogger.e(
+            'Failed to get announcements. Status: ${response.statusCode}, Body: ${response.data}');
+
+        throw Exception(
+            'Failed to get announcements. Status: ${response.statusCode}, Body: ${response.data}');
+      }
+    } on DioException catch (e) {
+      AppLogger.e('Dio error during get announcements: $e');
+      throw AppException.fromDioExceptionType(e.type);
+    } catch (e) {
+      AppLogger.e('Unexpected error during get announcements: $e');
       throw AppException(code: -1, message: 'Unexpected error occurred');
     }
   }
