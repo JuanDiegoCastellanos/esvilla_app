575f18d244d4649d2f787ce20967cb0833456922
Juan Diego Castellanos <juandiego.castellanosjerez@gmail.com>
2025-05-22 17:32:38 -0500

    JDC: Adding a new endpoint

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -2,7 +2,7 @@ enum Environment { development, production }
 
 class AppConfig{
   //static const String apiUrl = "https://esvilla-backend.onrender.com";
-  static const String apiUrl = "http://35.209.121.67:3000/"; 
+  static const String apiUrl = "http://35.223.80.90:3000/"; 
   //static const String apiUrl = "http://172.19.168.33:3000/"; 
   static const Environment environment = Environment.development;
   static const String pwd = "@Esvilla2025";
